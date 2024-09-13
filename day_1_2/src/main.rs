651a19d267392fec4a1c38a35d421f80d3d2ba32
lmlp playground <62958251+nilsen-hub@users.noreply.github.com>
2024-09-13 07:57:00 +0200

    Day 1.2 Finished (inelegant)

--- day_1_2/src/main.rs
+++ day_1_2/src/main.rs
@@ -4,26 +4,18 @@
 //Part 2 particulars: some numbers are written as "one" "two" etc. These also count
 
 use std::fs::read_to_string;
-use std::io;
 
 fn main() {
-    println!("Please enter filepath to list of bungled calibration values");
-    
-    let mut path = String::new();
-    io::stdin()
-        .read_line(&mut path)
-        .expect("Failed to read line");
-        
-    let path = path.trim();
+    let path = "./data/day_1_1";
 
     let data = get_list_from_file(path);
     let mut cal_val = Vec::new();
     
     for el in data{
-        let val = grab_number(el);
+        let translated_string = number_translator(el);
+        let val = grab_number(translated_string);
         cal_val.push(val);
     }
-
     let output = sum_vector(cal_val);
     println!("{output}");
 }
@@ -68,4 +60,24 @@ fn sum_vector(vector: Vec<u32>) -> u32 {
     }
 
     sum
+}
+
+fn number_translator(data: String) -> String {
+    let data = data.replace("one","onee");
+    let data = data.replace("two","twoo");
+    let data = data.replace("three","threee");
+    let data = data.replace("five","fivee");
+    let data = data.replace("seven","sevenn");
+    let data = data.replace("eight","eightt");
+    let data = data.replace("nine","ninee");
+    let data = data.replace("one","1");
+    let data = data.replace("two","2");
+    let data = data.replace("three","3");
+    let data = data.replace("four","4");
+    let data = data.replace("five","5");
+    let data = data.replace("six","6");
+    let data = data.replace("seven","7");
+    let data = data.replace("eight","8");
+    let data = data.replace("nine","9");
+    data
 }
---
+++ day_1_2/target/debug/day_1_2.d
@@ -0,0 +1,1 @@
+C:\Users\joach\Documents\code\rust\projects\advent_of_code\day_1_2\target\debug\day_1_2.exe: C:\Users\joach\Documents\code\rust\projects\advent_of_code\day_1_2\src\main.rs
