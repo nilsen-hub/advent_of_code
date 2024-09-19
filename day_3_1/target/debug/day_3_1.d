d7239316baa4ac93ee2bfb43d848a60b805e133c
lmlp playground <62958251+nilsen-hub@users.noreply.github.com>
2024-09-19 09:33:01 +0200

    Day 3_1 completed

---
+++ day_3_1/target/debug/day_3_1.d
@@ -0,0 +1,1 @@
+C:\Users\joach\Documents\code\rust\projects\advent_of_code\day_3_1\target\debug\day_3_1.exe: C:\Users\joach\Documents\code\rust\projects\advent_of_code\day_3_1\src\main.rs
---
+++ day_3_1/target/debug/deps/day_3_1.d
@@ -0,0 +1,5 @@
+C:\Users\joach\Documents\code\rust\projects\advent_of_code\day_3_1\target\debug\deps\day_3_1.exe: src/main.rs
+
+C:\Users\joach\Documents\code\rust\projects\advent_of_code\day_3_1\target\debug\deps\day_3_1.d: src/main.rs
+
+src/main.rs:
