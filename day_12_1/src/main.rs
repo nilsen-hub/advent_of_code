da696b18839e1cfb579015670364190c445075f2
nilsen-hub <62958251+nilsen-hub@users.noreply.github.com>
2024-11-15 15:45:06 +0100

    day 12 continued

--- day_12_1/src/main.rs
+++ day_12_1/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::read_to_string, iter::Map, time::Instant};
+use std::{collections::VecDeque, fs::read_to_string, iter::Map, time::Instant};
 
 #[derive(Debug, Clone)]
 struct SpringGroup {
@@ -21,7 +21,7 @@ struct ConditionMap {
 
 fn main() {
     let now = Instant::now();
-    let path = "./data/day12";
+    let path = "./data/day12T";
     let full_data = get_list_from_file(path);
     let mut value_accumulator: usize = 0;
     for line in full_data {
@@ -55,114 +55,91 @@ fn build_cm(maps: Maps) {
     // Fill and return struct
 }
 fn build_spr_groups(maps: Maps) {
-    // BUG UNCOVERED
-    // NEED TO COVER THIS CASE: ??.??????.?#????? 1,4
-    // set up definitions and workspace
-    // clone map_springs to have a workplace, and a clean reference
-    let map_springs = maps.springs;
-    let map_groups = maps.groups;
-    let mut working_vector = map_springs.clone();
-    // we're gonna be indexing around here, so need this to avoid panics
-    let bounds = map_springs.len();
-    let valid_symbols: [char; 2] = ['?', '#'];
-    let mut groups: Vec<SpringGroup> = Vec::with_capacity(8);
+    // -> Vec<SpringGroups>, base_arrangement (Vec<char>)
+    // The third time I'm attempting to build a decent function for this..
+    // Takes maps, and uses them to build a valid base arrangement of
+    // spring groups in the spring maps, also returns a vector of
+    // spring group coordinates.
 
-    // the goal of this function is to produce a left leaning valid
-    // first arrangement of the spring groups
+    // Builds a valid base case of spring groups
+    // The rules are 
+    
+    // - Groups must appear in order provided
+    // - No groups on '.'
+    // - All '#' must be covered by a group
+    // - '?' can be filled
+    // - No groups may be adjacent to another group
 
-    // Placement rules:
-    // '#' MUST be filled
-    // '?' can be filled
-    // no placement on '.' characters
-    // no two groups on adjacent indices
+    // this time around, to give some extra safety, i will consume
+    // placed groups into a new vector as I go, this should eliminate
+    // some errors encountered in the previous two attempts
+    // Actual function code and comments start below here:
+    // First set up a working enviroment and some helpful constants
+    // Reference, to have a clean map to compare to
+    // working vector to do process and analyze
+    // Vector to collect placed groups
+    // Also make groups variable, for semantic clarity
+    // Have valid symbols ready
+    // Instantiating vector which will hold the finished SpringGroup structs
+    let reference = maps.springs;
+    let mut working_vector = reference.clone();
+    let mut output_vector: Vec<char> = Vec::with_capacity(30);
+    let groups = maps.groups;
+    let valid_symbols: [char; 2] = ['?','#'];
+    let mut output: Vec<SpringGroup> = Vec::with_capacity(8);
+    println!("{:?}", reference);
 
-    // to enforce these rules, were going to assume all inputs are valid
-    // and then pass them through a series of "gates" to mark as false
-    // if needed, as we move through the data. This is probably a naive
-    // approach but for now its the best I've come up with. Theres a lot
-    // of cleanup to be done here, but this actually works
-
-    for (index, group_size) in map_groups.iter().enumerate() {
-        // loops through vector of map_groups and tests against map_springs
-        // for validity.
-        // index enumerator is used to determine group ID further down
-        // set size of window to pass over vector to match group size
-        let window_size = *group_size;
-        let mut window = working_vector.windows(window_size);
-        // initialize counter to keep track indices
+    for (index, group_size) in groups.iter().enumerate(){
+        // loops through groups, checks them against reference, and
+        // eventually transfers valid groups from working vector into
+        // output vector. Index is used as group ID in SpringGroup struct
+        // Define window matching size of current group to pass over 
+        // working Vector. 
+        // Instantiate counter for indexing.
+        // Instantiate bounds to avoid overflows
+        // Make group amount variable to help semantic clarity
+        // and start loop. 
+        let mut window = working_vector.windows(*group_size);
         let mut counter: usize = 0;
-        // initalize outer loop
-        'group_loop: loop {
-            // set valid_flag to true
-            let mut valid_flag: bool = true;
-            // start iterator over data
+        let bounds = working_vector.len();
+        let next_index = counter + group_size;
+        let group_amount = groups.len();
+        'group_loop: loop{
+            let mut valid_flag:bool = true;
+            // start or advance window
             let active_window = window.next().unwrap();
-            // loop over chars inside window, check for uniform validity of
-            // symbols in window
-            for el in active_window {
-                if valid_symbols.contains(el) == false {
+            // check that window contains all valid symbols
+            for el in active_window{
+                if valid_symbols.contains(el) == false{
                     valid_flag = false;
                     break;
                 }
             }
-            // check for trailing '#'-es, first check for overflow
-            if counter + active_window.len() < bounds
-                && working_vector[counter + active_window.len()] == '#'
-            {
+            // check for trailing '#', also check your not looking out of bounds
+            if next_index < bounds && working_vector[next_index] == '#'{
                 valid_flag = false;
             }
-            // check for adjacent numbers, also make sure to not underflow
-            if counter == 0 {
-                if working_vector[counter + active_window.len()].is_numeric() {
-                    valid_flag = false;
-                }
-            } else {
-                if working_vector[counter - 1].is_numeric()
-                    || working_vector[counter + 1].is_numeric()
-                {
-                    valid_flag = false;
-                }
-            }
-            // if every test is passed, roll out the group to its correct
-            // correct position in the working vector
+            // if both checks pass, the group has a valid placement.
+            // Insert group into working vector, split group and next index
+            // (if available) away from working vector, and append to 
+            // output_vector
             if valid_flag {
-                // setup for rollout of ID into working vector
-                let mut rollout = group_size.clone();
-                let group_id = char::from_digit(index.clone() as u32, 10).unwrap();
-                loop {
-                    if rollout == 0 {
-                        // final validity check of working vector
-                        if working_vector.contains(&'#') {
-                            for el in &working_vector {
-                                print!("{}", el);
-                            }
-                            println!("");
-                        }
-                        let spring_group = SpringGroup {
-                            id: index,
-                            size: *group_size,
-                            start_index: counter,
-                        };
-                        groups.push(spring_group);
-                        break 'group_loop;
+                if index + 1 == group_amount{
+                    output_vector.append(&mut working_vector);
+                } else {
+                    let (left, right) = working_vector.split_at(next_index + 1);
+                    for el in left {
+                        output_vector.push(*el);
                     }
-                    working_vector[counter + rollout - 1] = group_id;
-                    rollout -= 1;
+                    working_vector = (*right).to_vec();
                 }
+                println!("{:?}", output_vector);
+                break 'group_loop;
+
             }
-            if counter == bounds - window_size {
-                break;
-            }
-            counter += 1;
         }
     }
-
-    //for el in working_vector {
-    //    print!("{}", el);
-    //}
-    //println!("");
-
-    //(working_vector, groups)
+    // println!("{:?}", working_vector);
 }
 fn parse_line(line: String) -> (Vec<char>, Vec<usize>) {
     // Parses line from full data into types used in:
